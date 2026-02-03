127b46e4b2d049aa2548f89740cd7a95cb77efff
ompromedia-cyber <ompromedia@gmail.com>
2026-02-03 17:07:15 +0500

    Create icons.tsx

---
+++ src/components/ui/icons.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Lock = () => <span className="inline-block w-4 h-4">🔒</span>;
+export const Info = () => <span className="inline-block w-4 h-4">ℹ️</span>;
+export const Languages = () => <span className="inline-block w-4 h-4">🌐</span>;
+export const TrendingUp = () => <span className="inline-block w-5 h-5">📈</span>;
+export const RotateCcw = () => <span className="inline-block w-4 h-4">🔄</span>;
